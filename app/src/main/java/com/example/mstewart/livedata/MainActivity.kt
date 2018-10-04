a7f8d3576195c13fd75fe99e070dc23c846e696e
Mikey Stewart <>
2018-10-04 17:10:37 +1300

    add button that changes mutableLiveData

--- app/src/main/java/com/example/mstewart/livedata/MainActivity.kt
+++ app/src/main/java/com/example/mstewart/livedata/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.mstewart.livedata
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var mutableLiveData: MutableLiveData<String> = MutableLiveData()
+
+    init {
+        mutableLiveData.observe(this, Observer { newString ->
+            textView.text = newString
+        })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpButton()
     }
+
+    private fun setUpButton() {
+        button.setOnClickListener {
+            mutableLiveData.value = "Random int: ${Random().nextInt()}"
+        }
+    }
+
 }
